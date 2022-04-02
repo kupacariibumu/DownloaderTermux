421a07f04f813397397636c4670bb388d6aba460
Bobby Rahmanda <69358107+bobbyrahmanda13@users.noreply.github.com>
2022-04-02 17:33:38 +0700

    update youtube mp3 downloader

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 mkdir ../.shortcuts
 mv aria.sh ../.shortcuts
 mv ytbv.sh ../.shortcuts
+mv ytbmp3.sh ../.shortcuts
 chmod +x ../.shortcuts/aria.sh
 chmod +x ../.shortcuts/ytbv.sh
+chmod +x ../.shortcuts/ytbmp3.sh
