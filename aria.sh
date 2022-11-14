a0dc970ffbd3ff971af77599c9de8b122f17cd30
Bobby Rahmanda <69358107+bobbyrahmanda13@users.noreply.github.com>
2022-11-14 16:34:48 +0700

    update folder

--- aria.sh
+++ aria.sh
@@ -1,4 +1,4 @@
-nano urldownload/d.txt
-aria2c -c -s 16 -x 16 -k 1M -j 1 --max-download-limit=1M -i urldownload/d.txt -d storage/downloads
-#truncate -s 0 urldownload/d.txt
+nano DownloaderTermux/urldownload/d.txt
+aria2c -c -s 16 -x 16 -k 1M -j 1 --max-download-limit=1M -i DownloaderTermux/urldownload/d.txt -d storage/downloads
+#truncate -s 0 DownloaderTermux/urldownload/d.txt
 exit
