db49082806d194355acce9eb1b9f68d14af85a1a
Bobby Rahmanda <69358107+bobbyrahmanda13@users.noreply.github.com>
2022-11-14 16:56:52 +0700

    add

--- ytbv.sh
+++ ytbv.sh
@@ -6,6 +6,6 @@ exit
 else
 youtube-dl -f best -a /data/data/com.termux/files/home/DownloaderTermux/urldownload/links.txt --external-downloader "aria2c" --external-downloader-args "-c -s 16 -x 16 -k 1M -j 1 --max-download-limit=1M"
 youtube-dl --write-auto-sub --convert-subs=srt --sub-lang id --skip-download -a /data/data/com.termux/files/home/DownloaderTermux/urldownload/links.txt
-truncate -s 0 /data/data/com.termux/files/home/urldownload/links.txt
+truncate -s 0 /data/data/com.termux/files/home/DownloaderTermux/urldownload/links.txt
 exit
 fi
